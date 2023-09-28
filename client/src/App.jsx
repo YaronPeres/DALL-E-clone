fcf82387e6596ba69b6b650ff01a2127ea6f5019
Yaron Peres <yaronp16@gmail.com>
2023-09-28 21:00:56 +0200

    changes2

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import { logo } from "./assets";
-import { Home, CreatePost } from "@pages";
+import { Home, CreatePost } from "./pages";
 
 function App() {
   return (
