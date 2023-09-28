24e72bb6a6812acdf54645a4b4bfd47a337dfb3f
Yaron Peres <yaronp16@gmail.com>
2023-09-28 20:47:05 +0200

    changes2

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,14 @@
 // vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@pages": "src/pages",
+      "@pages": path.resolve("/src/pages"),
     },
   },
 });
