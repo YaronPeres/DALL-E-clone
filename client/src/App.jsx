90d564efa3d24a9ad10ee63ebdd7f7dfdc6abfec
Yaron Peres <yaronp16@gmail.com>
2024-02-17 19:58:19 +0100

    Fix vulnerabilities and update ip package

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -17,7 +17,7 @@ function App() {
           Create
         </Link>
       </header>
-      <main className="sm:p-8 px-4 py8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]">
+      <main className="sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/create-post" element={<CreatePost />} />
